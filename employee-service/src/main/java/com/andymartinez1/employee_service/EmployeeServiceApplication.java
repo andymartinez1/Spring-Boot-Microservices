607ceae6abb953baf3d077342dade9238a149178
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-04-23 11:52:38 -0400

    Updated to use Spring Webflux amd WebClient instead of RestTemplate as it is deprecated.

--- employee-service/src/main/java/com/andymartinez1/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/andymartinez1/employee_service/EmployeeServiceApplication.java
@@ -3,7 +3,7 @@ package com.andymartinez1.employee_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
@@ -13,8 +13,8 @@ public class EmployeeServiceApplication {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient.builder().build();
     }
 
 }
--- employee-service/src/main/java/com/andymartinez1/employee_service/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/andymartinez1/employee_service/service/impl/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -21,7 +22,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
@@ -35,9 +36,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(employeeId).get();
         EmployeeDTO employeeDTO = EmployeeMapper.mapToEmployeeDTO(employee);
 
-        ResponseEntity<DepartmentDTO> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"
-                + employee.getDepartmentCode(), DepartmentDTO.class);
-        DepartmentDTO departmentDTO = responseEntity.getBody();
+        DepartmentDTO departmentDTO = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDTO.class)
+                .block();
 
         APIResponseDTO apiResponseDTO = new APIResponseDTO();
         apiResponseDTO.setEmployee(employeeDTO);
